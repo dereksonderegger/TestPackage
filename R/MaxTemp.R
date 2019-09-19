f0426d6196cf72e0b914fb308b7343df5fdbf01d
Derek Sonderegger <derek.sonderegger@gmail.com>
2019-09-18 22:08:54 -0700

    Initial commit.

---
+++ R/MaxTemp.R
@@ -0,0 +1,9 @@
+#' A time series of daily maximum temperatures in Flagstaff, AZ. The data were
+#' downloaded from www.ncdc.noaa.gov.
+#'
+#' @format a data frame with 10882 observations
+#' \describe{
+#'   \item{Date}{The date of observation as a POSIX date format.}
+#'   \item{MaxTemp}{Daily maximum temperature in degrees Farhenheit.}
+#' }
+"MaxTemp"
